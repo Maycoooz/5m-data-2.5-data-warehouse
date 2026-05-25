f509f2ec93cdc95edc99d7e9bb23b1696451e5d3
Marcus <marrcuss00@gmail.com>
2026-05-25 14:34:44 +0800

    Assignment Completed

--- austin_bikeshare_demo/models/star/dim_station.sql
+++ austin_bikeshare_demo/models/star/dim_station.sql
@@ -1,8 +1,17 @@
-SELECT DISTINCT
-    station_id,
-    name,
-    status,
-    location,
-    address
-FROM {{ ref('station_snapshot')}}
-WHERE CURRENT_TIMESTAMP > dbt_valid_from AND dbt_valid_to IS NULL
+WITH current_stations AS (
+    SELECT *
+    FROM {{ ref('station_snapshot') }}
+    WHERE CURRENT_TIMESTAMP > dbt_valid_from AND dbt_valid_to IS NULL
+)
+
+SELECT
+    s.station_id,
+    s.name,
+    s.status,
+    s.location,
+    s.address,
+    AVG(t.duration_minutes) AS avg_duration
+FROM current_stations AS s
+LEFT JOIN {{ source('austin_bikeshare', 'bikeshare_trips') }} AS t
+ON t.start_station_id = s.station_id
+GROUP BY s.station_id, s.name, s.status, s.location, s.address
